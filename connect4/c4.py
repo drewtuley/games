fc239af1a24c27ba464a1c0f3af1c81f9e6d415f
drewtuley <drewtuley@gmail.com>
2017-07-21 16:12:48 +0100

    reformat/refactor

--- connect4/c4.py
+++ connect4/c4.py
@@ -9,7 +9,7 @@ TOP_ROW_IDX = 5
 RED = 1
 BLUE = 2
 GREEN = 3
-CHOICES = [x for x in range(0,7)]
+CHOICES = [x for x in range(0, 7)]
 
 board = [0 for ix in range(ROWS * COLS)]
 
@@ -36,12 +36,13 @@ def draw_piece(wx, game_row, game_col, colour):
     screen_row, screen_col = calc_screen_pos(game_row, game_col)
     wx.addch(screen_row, screen_col, curses.ACS_DIAMOND, colour)
 
+
 def is_move_valid(col):
     return get_piece(TOP_ROW_IDX, col) == 0
 
 
 def make_move(col, piece):
-    for rx in range(0,6):
+    for rx in range(0, 6):
         if get_piece(rx, col) == 0:
             set_piece(rx, col, piece)
             break
@@ -60,50 +61,53 @@ def swap_player(player):
 
 def find_winner():
     # check horizontal
-    for rx in ([0,1,2,3,4,5]):
-        for cx in ([0,1,2,3]):
+    for rx in ([0, 1, 2, 3, 4, 5]):
+        for cx in ([0, 1, 2, 3]):
             piece = get_piece(rx, cx)
-            if piece != 0 and get_piece(rx, cx+1) == piece and get_piece(rx, cx+2) == piece and get_piece(rx, cx+3) == piece:
-                return piece, (rx, cx), (rx, cx+1), (rx, cx+2), (rx, cx+3)
+            if piece != 0 and get_piece(rx, cx + 1) == piece and get_piece(rx, cx + 2) == piece and get_piece(rx,
+                                                                                                              cx + 3) == piece:
+                return piece, (rx, cx), (rx, cx + 1), (rx, cx + 2), (rx, cx + 3)
     # pure vertical
-    for rx in ([0,1,2]):
-        for cx in ([0,1,2,3,4,5,6]):
+    for rx in ([0, 1, 2]):
+        for cx in ([0, 1, 2, 3, 4, 5, 6]):
             piece = get_piece(rx, cx)
-            if piece != 0 and get_piece(rx+1, cx) == piece and get_piece(rx+2, cx) == piece and get_piece(rx+3, cx) == piece:
-                return piece, (rx, cx), (rx+1, cx), (rx+2, cx), (rx+3, cx)
+            if piece != 0 and get_piece(rx + 1, cx) == piece and get_piece(rx + 2, cx) == piece and get_piece(rx + 3,
+                                                                                                              cx) == piece:
+                return piece, (rx, cx), (rx + 1, cx), (rx + 2, cx), (rx + 3, cx)
     # forward slope
-    for rx in ([0,1,2]):
-        for cx in ([0,1,2,3]):
+    for rx in ([0, 1, 2]):
+        for cx in ([0, 1, 2, 3]):
             piece = get_piece(rx, cx)
-            if piece != 0 and get_piece(rx+1, cx+1) == piece and get_piece(rx+2, cx+2) == piece and get_piece(rx+3, cx+3) == piece:
-                return piece, (rx, cx), (rx+1, cx+1), (rx+2, cx+2), (rx+3, cx+3)
+            if piece != 0 and get_piece(rx + 1, cx + 1) == piece and get_piece(rx + 2, cx + 2) == piece and get_piece(
+                            rx + 3, cx + 3) == piece:
+                return piece, (rx, cx), (rx + 1, cx + 1), (rx + 2, cx + 2), (rx + 3, cx + 3)
 
     # backward slope    
-    for rx in ([0,1,2]):
-        for cx in ([3,4,5,6]):
+    for rx in ([0, 1, 2]):
+        for cx in ([3, 4, 5, 6]):
             piece = get_piece(rx, cx)
-            if piece != 0 and get_piece(rx+1, cx-1) == piece and get_piece(rx+2, cx-2) == piece and get_piece(rx+3, cx-3) == piece:
-                return piece, (rx, cx), (rx+1, cx-1), (rx+2, cx-2), (rx+3, cx-3)
-                
-       
+            if piece != 0 and get_piece(rx + 1, cx - 1) == piece and get_piece(rx + 2, cx - 2) == piece and get_piece(
+                            rx + 3, cx - 3) == piece:
+                return piece, (rx, cx), (rx + 1, cx - 1), (rx + 2, cx - 2), (rx + 3, cx - 3)
+
+
 def show_winner(winner):
     piece = winner[0]
     for w in winner[1:]:
         set_piece(w[0], w[1], GREEN)
-         
 
-def draw_board(wx, red_colour, blue_colour, green_colour):
+
+def draw_board(wx, c_map):
     wx.clear()
     wx.box()
     for rx in range(0, 6):
         for cx in range(0, 7):
             piece = get_piece(rx, cx)
-            if piece == RED:
-                draw_piece(wx, rx, cx, red_colour)
-            elif piece == BLUE:
-                draw_piece(wx, rx, cx, blue_colour)
-            elif piece == GREEN:
-                draw_piece(wx, rx, cx, green_colour)
+            try:
+                colour = c_map[piece]
+                draw_piece(wx, rx, cx, colour)
+            except KeyError:
+                pass
 
     wx.refresh()
 
@@ -120,6 +124,9 @@ def main(screen):
     colour2 = curses.color_pair(2)
     colour3 = curses.color_pair(3)
     colour4 = curses.color_pair(4)
+
+    colour_map = {RED: colour2, BLUE: colour3, GREEN: colour4}
+
     prev_curses = curses.curs_set(0)
 
     screen.refresh()
@@ -135,14 +142,14 @@ def main(screen):
             move = choice(CHOICES)
         make_move(move, player)
 
-        draw_board(win, colour2, colour3, colour4)
+        draw_board(win, colour_map)
         winner = find_winner()
         if winner is None:
             player = swap_player(player)
             time.sleep(0.01)
         else:
             show_winner(winner)
-            draw_board(win, colour2, colour3, colour4)
+            draw_board(win, colour_map)
 
     ch = screen.getch()
 
